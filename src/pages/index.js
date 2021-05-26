1e68da912da854593d36e8340f9922a9779b046c
FourSqweez <four7518@gmail.com>
2021-05-26 23:46:21 +0700

    ADD : Create Header component

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+export default function Home() {
+	return (
+		<div>
+			<Head>Amazon 2.0</Head>
+			<h1>Test</h1>
+			<Header/>
+		</div>
+	)
+}
