1e4f7ad51987d94e7da3d2386013b71f36c772fb
FourSqweez <four7518@gmail.com>
2021-05-27 15:23:15 +0700

    ADD : get data from fake api using serverside rendering

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
-export default function Home() {
+import ProductFeed from '../components/ProductFeed'
+export default function Home({ products }) {
 	return (
 		<div className="bg-gray-100">
 			<Head>Amazon 2.0</Head>
@@ -12,7 +13,19 @@ export default function Home() {
 				<Banner />
 
 				{/* ProductFeed */}
+				<ProductFeed products={products} />
 			</main>
 		</div>
 	)
+}
+
+export async function getServerSideProps(context) {
+	const products = await fetch(`https://fakestoreapi.com/products`).then(
+		(res) => res.json()
+	)
+	return {
+		props: {
+			products,
+		},
+	}
 }
