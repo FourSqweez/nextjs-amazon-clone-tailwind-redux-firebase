f4f1b37dfcd005267a7d1d097e90930462a5ee8a
FourSqweez <four7518@gmail.com>
2021-05-28 16:11:50 +0700

    ADD : firebase config

---
+++ firebase.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase'
+const firebaseConfig = {
+	apiKey: 'AIzaSyA7GEsy1iLLdcUaBcrt7m5HOTCY1VMDKys',
+	authDomain: 'tailwind-redux-nextauth.firebaseapp.com',
+	projectId: 'tailwind-redux-nextauth',
+	storageBucket: 'tailwind-redux-nextauth.appspot.com',
+	messagingSenderId: '281759252744',
+	appId: '1:281759252744:web:25c3494525d65c6982acbd',
+}
