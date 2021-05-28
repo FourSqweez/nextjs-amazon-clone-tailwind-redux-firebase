e2aca3eab88ac6fb3008077e33c19e89965b4aee
FourSqweez <four7518@gmail.com>
2021-05-28 18:17:12 +0700

    ADD : Building chckout page

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,9 +1,48 @@
+import Image from 'next/image'
 import Header from './../components/Header'
+import { useSelector } from 'react-redux'
+import { selectItems } from './../slices/basketSlice'
+import CheckoutProduct from './../components/CheckoutProduct'
 export default function Checkout() {
+	const items = useSelector(selectItems)
 	return (
 		<div className="bg-gray-100">
 			<Header />
-         <h1>Check Page</h1>
+			<main className="lg:flex max-w-screen-xl mx-auto">
+				{/* Left */}
+				<div className="flex-grow m-5 shadow-sm">
+					<Image
+						src="https://links.papareact.com/ikj"
+						width={1020}
+						height={250}
+						objectFit="contain"
+					/>
+
+					<div className="flex flex-col p-5 space-y-10 bg-white">
+						<h1 className="text-3xl border-b pb-4 ">
+							{items.length === 0
+								? 'Your Amazon Basket is empty.'
+								: 'Shopping Basket'}
+						</h1>
+						{items.map((item, i) => (
+							<CheckoutProduct
+								key={i}
+								id={item.id}
+								title={item.title}
+								price={item.price}
+								rating={item.rating}
+								description={item.description}
+								category={item.category}
+								image={item.image}
+								hasPrime={item.hasPrime}
+							/>
+						))}
+					</div>
+				</div>
+
+				{/* Right */}
+				<div></div>
+			</main>
 		</div>
 	)
 }
