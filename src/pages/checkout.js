5888e4104105d1a4953dfbe5927979136517c66e
FourSqweez <four7518@gmail.com>
2021-05-28 16:25:48 +0700

    ADD : Created checkout page

---
+++ src/pages/checkout.js
@@ -0,0 +1,9 @@
+import Header from './../components/Header'
+export default function Checkout() {
+	return (
+		<div className="bg-gray-100">
+			<Header />
+         <h1>Check Page</h1>
+		</div>
+	)
+}
